89c5e54fdca4aab1b4ec9b9fcffbb5f4450c9194
Aiman <aimnabuleil.uni@gmail.com>
2025-10-23 15:54:45 +0300

    Pages init

---
+++ src/Pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+function About() {
+  return (
+    <div>
+      <h1>ABOUT Us</h1>
+      <p>This is the ABOUT Page. You can add ABOUT.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Pages/Admin.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Admin() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>This is the ADMIN.</p>
+    </div>
+  );
+}
+
+export default Admin;
